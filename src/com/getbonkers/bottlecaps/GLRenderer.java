40dd5d96a4704957fa57ed413ae9b466cb4c75d5
Marc Chambers <chambers.marc@gmail.com>
2012-01-11 19:16:58 -0800

    yea-yuh!!!

---
+++ src/com/getbonkers/bottlecaps/GLRenderer.java
@@ -0,0 +1,18 @@
+package com.getbonkers.bottlecaps;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class GLRenderer implements Renderer {
+
+	public void onDrawFrame(GL10 gl) {
+	}
+
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+	}
+
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+	}
+}
